7ae655db1a81c6030e2f1011bbba7776685beacb
Konrad P <kokiziom@gmail.com>
2023-05-18 22:09:54 +0200

    Added get single note and create note

--- notes_api/urls.py
+++ notes_api/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.notes_view, name="notes"),
+    path('note/<int:pk>/', views.get_note, name="get-note"),
+    path('add/', views.add_note, name="add-note"),
 ]
--- notes_api/views.py
+++ notes_api/views.py
@@ -8,3 +8,23 @@ def notes_view(request):
     notes = Note.objects.all()
     serializer = NoteSerializer(notes, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET'])
+def get_note(request, pk):    
+    note = Note.objects.get(pk=pk)
+    serializer = NoteSerializer(note, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def add_note(request):
+    serializer = NoteSerializer(data= request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+# @api_view(['PUT'])
+# def update_note(request, pk):
+#     note = Note.objects.get(pk=pk)
+#     serializer = NoteSerializer(note, many=False)
