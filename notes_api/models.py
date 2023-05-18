1c6d32602ac5c196e41edaadf884482c5546ddb0
Konrad P <kokiziom@gmail.com>
2023-05-18 22:03:35 +0200

    Added Note model

--- notes_api/models.py
+++ notes_api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Note(models.Model):
+    content = models.TextField(null= True, blank= True)
+    creation_date = models.DateTimeField(auto_now_add= True)
+    update_date = models.DateTimeField(auto_now= True)
+
+    def __str__(self):
+        return f'{self.content[0:50]}'
