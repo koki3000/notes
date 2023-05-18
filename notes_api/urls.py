7a8bf8a8d5980700e9ba573e3ef8fbd99fde0db8
Konrad P <kokiziom@gmail.com>
2023-05-18 22:41:54 +0200

    Added update and delete note

--- notes_api/urls.py
+++ notes_api/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.notes_view, name="notes"),
     path('note/<int:pk>/', views.get_note, name="get-note"),
-    path('add/', views.add_note, name="add-note"),
+    path('note/add/', views.add_note, name="add-note"),
+    path('note/update/<int:pk>/', views.update_note, name="update-note"),    
+    path('note/delete/<int:pk>/', views.delete_note, name="delete-note"),
 ]
--- notes_api/views.py
+++ notes_api/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 from .serializers import NoteSerializer
 from rest_framework.response import Response
 
+
 @api_view(['GET'])
 def notes_view(request):
     notes = Note.objects.all()
@@ -12,7 +13,7 @@ def notes_view(request):
 
 @api_view(['GET'])
 def get_note(request, pk):    
-    note = Note.objects.get(pk=pk)
+    note = Note.objects.get(id=pk)
     serializer = NoteSerializer(note, many=False)
     return Response(serializer.data)
 
@@ -24,7 +25,19 @@ def add_note(request):
         serializer.save()
     return Response(serializer.data)
 
-# @api_view(['PUT'])
-# def update_note(request, pk):
-#     note = Note.objects.get(pk=pk)
-#     serializer = NoteSerializer(note, many=False)
+
+@api_view(['PUT'])
+def update_note(request, pk):
+    data = request.data
+    note = Note.objects.get(id=pk)
+    serializer = NoteSerializer(instance= note, data= data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def delete_note(request, pk):
+    note = Note.objects.get(id=pk)
+    note.delete()
+    return Response('Note has been deleted.')
