16b1565105ccda8b787167b41f11ae543858209c
Konrad P <kokiziom@gmail.com>
2023-05-18 22:05:46 +0200

    Added get notes view

--- notes_api/views.py
+++ notes_api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from .models import Note
+from rest_framework.decorators import api_view
+from .serializers import NoteSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET'])
+def notes_view(request):
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
