4c620922898ae1f05578a6e0cc483b305d51a756
Konrad P <kokiziom@gmail.com>
2023-05-21 16:38:51 +0200

    Added title field to note model

--- notes_api/models.py
+++ notes_api/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Note(models.Model):
+    title = models.TextField(null= True, blank= True)
     content = models.TextField(null= True, blank= True)
     creation_date = models.DateTimeField(auto_now_add= True)
     update_date = models.DateTimeField(auto_now= True)
